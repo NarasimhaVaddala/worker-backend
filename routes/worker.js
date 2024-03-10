3a99daf47654ceb79a57a4285f7c9a2a7ad8e124
NarasimhaVaddala <vaddalanarasimha@gmail.com>
2024-03-10 23:33:19 +0530

    Nothing Much but consolelogs in workerjs file

--- routes/worker.js
+++ routes/worker.js
@@ -10,7 +10,7 @@ router.post('/addnewworker', isLogin, async (req, res) => {
         const { name, mobile, designation, rate } = req.body;
         let adminid = req.user;
         const newWorker = await workermodel.create({ name, mobile, designation, rate , adminid })
-        console.log(newWorker);
+        console.log("newWorker : " , newWorker);
         return res.status(200).send({ success: true, worker: newWorker })
     }
     catch (e) {
@@ -33,6 +33,7 @@ router.get('/fetchallworkers', isLogin, async (req, res) => {
     try {
         let adminid = req.user;
         const workerlist = await workermodel.find({adminid});
+        console.log(workerlist);
         return res.status(200).send({ workerlist, success: true })
     } catch (error) {
         return res.status(500).send({ error: "Internal Server Error", success: false })
@@ -46,7 +47,7 @@ router.delete('/deleteworker', isLogin, async (req, res) => {
         const { id } = req.body;
         let adminid = req.user;
         const deletedworker = await workermodel.deleteOne({ _id: id , adminid:adminid})
-
+        console.log("deletedworker : ", deletedworker);
         return res.status(200).send({ success: true, deletedworker })
     } catch (e) {
         console.log(e.message);
@@ -59,7 +60,7 @@ router.put('/editworker', isLogin, async (req, res) => {
         const { id, name, designation, mobile, rate } = req.body;
 
         const editedWorker = await workermodel.findByIdAndUpdate(id, { name, designation, mobile, rate });
-        console.log(editedWorker);
+        console.log("editedWorker : ", editedWorker );
         return res.status(200).send({ success: true, editedWorker })
     } catch (e) {
         console.log(e.message);
@@ -69,28 +70,28 @@ router.put('/editworker', isLogin, async (req, res) => {
 })
 
 router.put('/takeattendance/:id' , isLogin , async(req,res)=>{
-        try {
-            const {time , advance , date} = req.body;
-           
-            const id = req.params.id;
-            
-            const editedWorker = await workermodel.updateOne({_id:id}, {$push:{attendance:{time , advance , date}}});
-            console.log(editedWorker);
-        } catch (e) {
-            console.log(e.message);
-            return res.status(400).send({ error: "Something went wrong", success: false })
-        }
+    try {
+        const {time , advance , date} = req.body;
+       
+        const id = req.params.id;
+        
+        const editedWorker = await workermodel.updateOne({_id:id}, {$push:{attendance:{time , advance , date}}});
+        console.log(editedWorker);
+    } catch (e) {
+        console.log(e.message);
+        return res.status(400).send({ error: "Something went wrong", success: false })
+    }
 })
 
 router.post('/getatt' , isLogin , async(req,res)=>{
-    try {
-            const {id } = req.body;
-            const attendance = await workermodel.findOne({_id : id})
-            console.log(attendance.attendance);
-           return res.status(200).send({success:true , attendance:attendance.attendance})
-    } catch (error) {
-        return res.status(400).send({ error: "Something went wrong", success: false })
-    }
+try {
+        const {id } = req.body;
+        const attendance = await workermodel.findOne({_id : id})
+        console.log(attendance.attendance);
+       return res.status(200).send({success:true , attendance:attendance.attendance})
+} catch (error) {
+    return res.status(400).send({ error: "Something went wrong", success: false })
+}
 })
 
 
